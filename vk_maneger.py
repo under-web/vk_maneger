9566df7c209fd34ec08fdef5f5f3318f8cca8d38
Furens <furens@protonmail.com>
2023-01-27 14:35:33 +0300

    поправил код

--- vk_maneger.py
+++ vk_maneger.py
@@ -1,29 +1,36 @@
+import time
+
 import vk_api
 import random
 
-def get_random_message():
-    with open('messages.txt', 'r', encoding='utf8', errors='ignore') as txt_file:
-        return random.choice(txt_file.readlines())
 
-def get_friends_public():
-    with open('groups.txt', 'r', encoding='utf8', errors='ignore') as txt_file:
-        return txt_file.readlines()
-
-def post_wall_vk(login, password):
+def post_wall_vk(login, password, list_publics, messages):
     vk_session = vk_api.VkApi(login, password)
     vk_session.auth()
 
-    for owner_id in get_friends_public():
+    upload = vk_api.VkUpload(vk_session)
+
+    for i in list_publics:
+        print(f"Отправляю в {i.strip()}")
         vk_session.method("wall.post", {
-            'owner_id': owner_id,
-            'message': get_random_message(),
+            'owner_id': i,
+            'message': messages,
             'attachment': '',
         })
+        time.sleep(random.randint(10, 20))
 
+with open('messages.txt', 'r', encoding='utf8', errors='ignore') as txt_file:
+    messages =  random.choice(txt_file.readlines())
+
+
+with open('groups.txt', 'r', encoding='utf8', errors='ignore') as txt2_file:
+    list_publics = txt2_file.readlines()
 
 with open('config.txt', 'r', encoding='utf8', errors='ignore') as txt_file:
     auth = txt_file.readlines()
 try:
-    post_wall_vk(login=auth[0], password=auth[1])
+
+    post_wall_vk(login=auth[0].strip(), password=auth[1].strip(), list_publics=list_publics, messages=messages)
 except Exception as err:
     print('Ошибка', err)
+    input()
